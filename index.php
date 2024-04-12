bdc21818c905206e5aae4bf70c96728b36fe5c57
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-12 15:23:49 +0200

    [Working] Add Bootstrap + Font Awesome for some style

--- index.php
+++ index.php
@@ -1,18 +1,51 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/3a46370e2f.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Php - Badwords</title>
 </head>
+
 <body>
-    <form action="myscript.php" method="get">
-        <label for="paragraph_text">Inserisci qui il paragrafo</label><br>
-        <textarea cols="50" rows="10" name="paragraph_text" ></textarea><br><br>
-        <label for="badword">Inserisci una parola da censurare: </label>
-        <input type="text" placeholder="Dimmi una parolaccia..." name="badword">
-        <button type="submit">Invia</button>
-    </form>
+
+    <header class="mb-3">
+        <nav class="navbar bg-warning">
+            <div class="container-fluid justify-content-center">
+                <a class="navbar-brand" href="#">
+                    Censuratore <i class="fa-solid fa-poop"></i>
+                </a>
+            </div>
+        </nav>
+    </header>
+
+    <div class="container">
+
+        <form action="myscript.php" method="get">
+            <div class="m-auto">
+                <label class="form-label" for="paragraph_text">Inserisci qui un paragrafo ricco di brutte parole:</label><br>
+                <!--name="paragraph_text(variabile)"-->
+                <textarea class="form-control" cols="50" rows="10" name="paragraph_text"></textarea><br><br>
+            </div>
+            <div class="m-auto">
+                <label class="form-label" for="badword">Inserisci una parola da censurare: </label>
+                <!--name="badword(variabile)"-->
+                <input class="form-control" type="text" placeholder="Dimmi una parolaccia..." name="badword">
+            </div>
+            <button type="submit" class="btn btn-warning mt-1">Invia</button>
+        </form>
+
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz"
+        crossorigin="anonymous"></script>
+
 </body>
+
 </html>
--- myscript.php
+++ myscript.php
@@ -2,25 +2,57 @@
 
 //$message = "Hello PHP";
 
+//Variabile Globale
 var_dump($_GET);
 $paragraph_text = "$_GET[paragraph_text]";
 $badword = "$_GET[badword]";
 $censored_text = str_replace(strtolower($badword), "***", strtolower($paragraph_text))
 
-?>
+    ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
+
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/3a46370e2f.js" crossorigin="anonymous"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <title>Php - Badwords</title>
 </head>
+
 <body>
-    
-    <h1><?php echo $paragraph_text ?></h1>
-    <h1>Il paragrafo contiene: <?php echo strlen($paragraph_text) ?> lettere</h1>
-    <h1><?php echo $censored_text; ?></h1>
-    <h1>Il paragrafo contiene: <?php echo strlen($censored_text) ?> lettere</h1>
+    <header class="mb-3">
+        <nav class="navbar bg-warning">
+            <div class="container-fluid justify-content-center">
+                <a class="navbar-brand" href="#">
+                    Censuratore <i class="fa-solid fa-poop"></i>
+                </a>
+            </div>
+        </nav>
+    </header>
+    <div class="container">
+
+        <div class="outputs">
+
+            <h1><?php echo $paragraph_text ?></h1>
+            <h2>Il paragrafo contiene: <?php echo strlen($paragraph_text) ?> lettere</h2>
+
+
+        </div>
+
+        <div class="outputs mt-5">
+
+
+            <h1><?php echo $censored_text; ?></h1>
+            <h2>Il paragrafo contiene: <?php echo strlen($censored_text) ?> lettere</h2>
+
+        </div>
+
+    </div>
+
 </body>
+
 </html>
