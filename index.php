fbdae67731cc8f7cf74878432e35865dcb81abbf
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-04-12 14:41:50 +0200

    [Working] I can Print all the things now  + New file created

--- index.php
+++ index.php
@@ -19,9 +19,9 @@ $badword = "$_GET[badword]";
 <body>
     <h1><?php echo $paragraph_text ?></h1>
     <h1>Il paragrafo contiene: <?php echo strlen($paragraph_text) ?> parole</h1>
-    <h1><?php echo $badword ?></h1>
+    <h1><?php echo str_replace($badword, "***", $paragraph_text) ?></h1>
 
-    <form action="" method="get">
+    <form action="myscript.php" method="get">
         <textarea name="paragraph_text" cols="50" rows="10"></textarea><br><br>
         <input type="text" placeholder="Dimmi una parolaccia..." name="badword">
         <button type="submit">Invia</button>
